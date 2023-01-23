20923c07ca0fe178efa260322d20755495651a96
erik <ekalle@ttu.ee>
2023-01-23 15:37:41 +0200

    (issue 26) added queue support for database and reimplemented schema

--- app/models/model.py
+++ app/models/model.py
@@ -1,13 +1,18 @@
-from sqlalchemy import Integer, Column, ForeignKey, DateTime, func
+from sqlalchemy import Integer, Column, ForeignKey, DateTime, func, BigInteger
 from sqlalchemy.orm import relationship
 
 from app import db
 
 
 class Model(db.Model):
-    id = Column(Integer, primary_key=True)
+    id = Column(BigInteger, primary_key=True)
+    parent_model_id = Column(BigInteger, ForeignKey('model.id'), nullable=True)
     added = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     model_status_id = Column(Integer, ForeignKey("model_status.id"), nullable=False)
+    project_id = Column(BigInteger, ForeignKey("project.id"), nullable=False)
+    latest_batch_id = Column(BigInteger, nullable=False)
 
     model_results_id = relationship("ModelResults", backref="model")
-    images = relationship("Image", secondary="model_image", back_populates="models")
+    parent = relationship('Model', remote_side=[id])
+
+    # images = relationship("Image", secondary="model_image", back_populates="models")
