e609162ad8267a5be932837747429bebb90efaab
erik <ekalle@ttu.ee>
2023-01-26 13:48:50 +0200

    (issue 36) updated models

--- app/models/model.py
+++ app/models/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, Column, ForeignKey, DateTime, func, BigInteger
+from sqlalchemy import Integer, Column, ForeignKey, DateTime, func, BigInteger, LargeBinary
 from sqlalchemy.orm import relationship
 
 from app import db
@@ -10,11 +10,10 @@ class Model(db.Model):
     added = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     model_status_id = Column(Integer, ForeignKey("model_status.id"), nullable=False)
 
-    latest_batch = Column(BigInteger, nullable=False)
-
     project_id = Column(BigInteger, ForeignKey("project.id", name="fk_project"), nullable=False)
+    model = Column(LargeBinary, nullable=True)
 
     model_results_id = relationship("ModelResults", backref="model")
     parent = relationship('Model', remote_side=[id])
 
-    # images = relationship("Image", secondary="model_image", back_populates="models")
+    images = relationship("Image", secondary="model_image", back_populates="models")
--- app/models/project.py
+++ app/models/project.py
@@ -8,11 +8,10 @@ class Project(db.Model):
     id = Column(BigInteger, primary_key=True)
     name = Column(VARCHAR(128), nullable=False, unique=True)
 
-    latest_batch = Column(BigInteger, nullable=False)
-
     latest_model_id = Column(BigInteger, ForeignKey("model.id", name="fk_latest_model"), nullable=True)
 
     queue = relationship('Queue', backref='project', lazy=True, uselist=False)
+    project_settings = relationship("ProjectSettings", backref='project', lazy=True, uselist=False)
     image_classes = relationship("ImageClass", backref="project")
     images = relationship("Image", backref="project")
     annotations = relationship("Annotation", backref="project")
