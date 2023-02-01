36288cbb3c37ded611b60522cd6e7511f1a0e633
erik <ekalle@ttu.ee>
2023-02-01 15:20:16 +0200

    (issue 32) created better project layout

--- app/models/model_results.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Integer, Column, BigInteger, ForeignKey
-
-from app import db
-
-
-class ModelResults(db.Model):
-    id = Column(Integer, primary_key=True)
-    model_id = Column(BigInteger, ForeignKey("model.id"), nullable=False)
---
+++ project/models/project.py
@@ -0,0 +1,24 @@
+from sqlalchemy import VARCHAR, Column, Integer, BigInteger, ForeignKey, UniqueConstraint
+from sqlalchemy.orm import relationship
+
+from project import db
+
+
+class Project(db.Model):
+    id = Column(BigInteger, primary_key=True)
+    name = Column(VARCHAR(128), nullable=False, unique=True)
+
+    latest_model_id = Column(BigInteger, ForeignKey("model.id", name="fk_latest_model"), nullable=True)
+
+    queue = relationship('Queue', backref='project', lazy=True, uselist=False)
+    project_settings = relationship("ProjectSettings", backref='project', lazy=True, uselist=False)
+    image_classes = relationship("ImageClass", backref="project")
+    images = relationship("Image", backref="project")
+    annotations = relationship("Annotation", backref="project")
+    models = relationship("Model", backref="project", primaryjoin="Project.id == Model.project_id")
+
+    latest_model = relationship("Model",
+                                foreign_keys=[latest_model_id],
+                                uselist=False,
+                                primaryjoin="Model.id==Project.latest_model_id",
+                                post_update=True)
