cedbb11aa05c1c0a68f849a5146308118a709d16
karl <kkuusi@ttu.ee>
2023-02-16 15:28:31 +0200

    (issue 63) Added project id to /projects ['GET']

--- project/schemas/project.py
+++ project/schemas/project.py
@@ -2,5 +2,6 @@ from marshmallow import Schema, fields
 
 
 class Project(Schema):
+    id = fields.Integer(required=True)
     name = fields.String(required=True)
     max_class_nr = fields.Integer(required=True)
