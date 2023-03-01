1be4cff355d928c7b1cc18484a04cec9523dbf36
erik <ekalle@ttu.ee>
2023-03-01 18:01:20 +0200

    (issue 100) fileupload with stream

--- project/views/project.py
+++ project/views/project.py
@@ -1,10 +1,13 @@
+import cgi
 import io
+import re
 import tarfile
 from io import BytesIO
 
 import PIL
 from flask import Blueprint, request, jsonify
 from marshmallow import ValidationError
+from werkzeug.datastructures import MultiDict
 from werkzeug.utils import secure_filename
 
 from project.services.file_upload_service import upload_files
@@ -37,6 +40,7 @@ def _text_to_annotations(content, filename):
     _list = []
     name = filename.split(".")[0]
     text = str(content, "utf-8")
+    # print(text)
     for line in text.splitlines():
         try:
             nr, x, y, w, h = line.strip().split(" ")
@@ -70,24 +74,6 @@ def _bytes_to_image(content, filename):
         raise ValidationError({"error": f"Can't read file: {filename}"})
     name = filename.split(".")[0]
     return Image(image=content, width=img.size[0], height=img.size[1]), name
-
-
-def _check_files(files):
-    final_files = []
-    for file in files:
-        filename = secure_filename(file.filename)
-        content = file.stream.read()
-        file.stream.close()
-        if filename.endswith(".txt"):
-            annotations = _text_to_annotations(content, filename)
-            final_files.extend(annotations)
-        elif filename.endswith(".png") or filename.endswith(".jpg"):
-            final_files.append(_bytes_to_image(content, filename))
-        elif filename.endswith(".tar.gz"):
-            final_files.extend(_check_zip_file(content))
-        else:
-            raise ValidationError({'error': f'not supported parsing {filename}'})
-    return final_files
 
 
 def _check_zip_file(content):
@@ -133,27 +119,185 @@ def get_projects():
 
     return serialized_projects
 
+def _type_and_bytes_to_objects(filename, _bytes, values):
+    if filename is None:
+        return
+    filename = secure_filename(filename)
+    if filename.endswith(".txt"):
+        annotations = _text_to_annotations(_bytes, filename)
+        values.extend(annotations)
+    elif filename.endswith(".png") or filename.endswith(".jpg"):
+        values.append(_bytes_to_image(_bytes, filename))
+    elif filename.endswith(".tar.gz"):
+        values.extend(_check_zip_file(_bytes))
+    else:
+        raise ValidationError({'error': f'not supported parsing {filename}'})
+
+
+def _get_name(text_bytes):
+    text = text_bytes.decode('utf-8')
+    match = re.search(r'name="([^"]+)"', text)
+    if match:
+        value = match.group(1)
+        return value
+    return None
+
+def _get_filename(text_bytes):
+    text = text_bytes.decode('utf-8')
+    match = re.search(r'filename="([^"]+)"', text)
+    if match:
+        value = match.group(1)
+        return value
+    return None
+def _validate_stream(flask_request):
+
+    required = {
+        "uploader_name": None,
+        "split": None,
+        "files": []
+    }
+
+    # get boundary between files in form
+    content_type = flask_request.headers.get('Content-Type')
+    _, params = cgi.parse_header(content_type)
+    boundary = params.get('boundary').encode()
+
+    # boundary in header is different from the one in stream
+    # i dont know why
+    # its usually 2 b"-" shorter
+    boundary = b"--" + boundary
+    end = boundary + b"--"
+
+    i = 100_000
+    next_lines_are_file = None
+    b_list = bytearray()
+    values = []
+    while i:
+        line = flask_request.stream.readline()
+        if line.strip() == boundary:
+            _type_and_bytes_to_objects(next_lines_are_file, b_list, values)
+
+            text = request.stream.readline().strip()
+            _ = request.stream.readline().strip()
+            value = request.stream.readline().strip()
+
+            name = _get_name(text)
+            filename = _get_filename(text)
+            # _type = _type.decode('utf-8').replace("Content-Type: ", "")
+            value = value.decode('utf-8')
+            next_lines_are_file = filename
+            b_list = bytearray()
+
+            if name in required:
+                required[name] = value
+            else:
+                raise ValidationError({'error': f'unknown field: {name}'})
+        elif line.strip() == end:
+            _type_and_bytes_to_objects(next_lines_are_file, b_list, values)
+            break
+        elif line == b"":
+            raise ValidationError({'error': f'cant read request'})
+        elif next_lines_are_file:
+            b_list.extend(line)
+        i -= 1
+    else:
+        print("error")
+    # for i in required:
+    #     print(i, required[i])
+    # print(values)
+    #
+    # print(flask_request.values)
+    # print(flask_request.headers)
+    # print(flask_request)
+
+    # files = _check_zip_file(b_list)
+    # print(files)
 
 @REQUEST_API.route('/<int:project_id>/upload', methods=["POST"])
 def upload(project_id: int):
-    data = request.form
-    errors = Upload().validate(data)
 
-    if errors:
-        return jsonify({'error': f'Please check the following fields: {errors}'}), 400
+    files = _validate_stream(request)
+    print(files)
 
-    uploader = data["uploader_name"]
-    split = data["split"]
-    files = request.files.getlist("files")
-    if files is None:
-        return jsonify({'error': f'Files field not found'}), 400
-    uploaded_files = _check_files(files)
-    if type(uploaded_files) is tuple:
-        return uploaded_files
-    passed, failed, annotations = upload_files(uploaded_files, project_id, uploader, split)
+    # # get boundary between files in form
+    # content_type = request.headers.get('Content-Type')
+    # _, params = cgi.parse_header(content_type)
+    # boundary = params.get('boundary').encode()
 
-    return jsonify(
-        {'message': f'Uploaded {passed} images and {annotations} annotations. There were {failed} failed images'}), 201
+
+
+
+        # break
+        # print(boundary)
+    #     print(line)
+    #     print(line == boundary)
+    #     if line.strip() == boundary:
+    #         break
+    #     break
+    # print("eee")
+    # while True:
+    #     line = request.stream.readline()
+    #     if not line.strip():
+    #         break
+    #     key, value = line.decode().strip().split('; ')
+    #     key = key.split('"')[1]
+    #     if key == 'uploader_name':
+    #         uploader_name = value.split('"')[1]
+    #     elif key == 'split':
+    #         split = value.split('"')[1]
+    #     else:
+    #         form_data.add(key, value.split('"')[1])
+    # with open('/path/to/save/file', 'wb') as f:
+    #     while True:
+    #         line = request.stream.readline()
+    #         if line.strip() == boundary:
+    #             break
+    #     while True:
+    #         headers = {}
+    #         while True:
+    #             line = request.stream.readline()
+    #             if not line.strip():
+    #                 break
+    #             key, value = line.decode().strip().split(': ')
+    #             headers[key.lower()] = value
+    #         if not headers:
+    #             break
+    #         chunk_size = int(headers.get('content-length', 0))
+    #         while chunk_size > 0:
+    #             chunk = request.stream.read(min(chunk_size, chunk_size))
+    #             if not chunk:
+    #                 break
+    #             f.write(chunk)
+    #             chunk_size -= len(chunk)
+    return jsonify({'status': 'success'}), 200
+
+    # print("eee")
+    # f = request.files
+    # print("eeer")
+    # print(f)
+    # for f in a:
+    #     print(f)
+    # data = request.form
+    # print("233232")
+    # errors = Upload().validate(data)
+    #
+    # if errors:
+    #     return jsonify({'error': f'Please check the following fields: {errors}'}), 400
+    #
+    # uploader = data["uploader_name"]
+    # split = data["split"]
+    # print("000")
+    # files = request.files.getlist("files")
+    # print("111")
+    # if files is None:
+    #     return jsonify({'error': f'Files field not found'}), 400
+    # uploaded_files = _check_files(files)
+    # if type(uploaded_files) is tuple:
+    #     return uploaded_files
+    # passed, failed, annotations = upload_files(uploaded_files, project_id, uploader, split)
+    return jsonify("done"), 200
+    # return jsonify(
+    #     {'message': f'Uploaded {passed} images and {annotations} annotations. There were {failed} failed images'}), 201
 
 
 @REQUEST_API.route('/<int:project_id>', methods=['GET'])
