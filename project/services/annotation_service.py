2202b082b8ae6c7da4a6b364980759ae32e9bdaa
karl <kkuusi@ttu.ee>
2023-03-16 13:27:02 +0200

    (issue 120) Fixed annotation info endpoint

--- project/services/annotation_service.py
+++ project/services/annotation_service.py
@@ -1,28 +1,28 @@
 from project.models.annotation import Annotation
+from project.models.annotation_extra import AnnotationError
 from project.models.annotator import Annotator
 from project import db
 from marshmallow import ValidationError
 
 
 def retrieve_annotation(annotation_code):
-    annotation = db.session.query(Annotation, Annotator).join(Annotator,
-                                                              Annotation.annotator_id == Annotator.id).filter(
-        Annotation.id == annotation_code).first()
+    annotation_info = db.session.query(Annotation, Annotator). \
+        join(Annotator.annotations) \
+        .filter(Annotation.id == annotation_code).first()
 
-    if not annotation:
+    if not annotation_info:
         return None
 
     annotation_dict = {
-        'id': annotation[0].id,
-        'confidence': annotation[0].confidence,
-        'x_center': annotation[0].x_center,
-        'y_center': annotation[0].y_center,
-        'width': annotation[0].width,
-        'height': annotation[0].height,
-        'class_id': annotation[0].class_id,
-        'project_id': annotation[0].project_id,
-        'image_id': annotation[0].image_id,
-        'annotator_name': annotation[1].name
+        'id': annotation_info[0].id,
+        'x_center': annotation_info[0].x_center,
+        'y_center': annotation_info[0].y_center,
+        'width': annotation_info[0].width,
+        'height': annotation_info[0].height,
+        'class_id': annotation_info[0].class_id,
+        'project_id': annotation_info[0].project_id,
+        'image_id': annotation_info[0].image_id,
+        'annotator_name': annotation_info[1].name
     }
 
     return annotation_dict
