2f207f0c92bd841650ff31aca7aacd887e6a26c7
erik <ekalle@ttu.ee>
2023-04-03 16:27:54 +0300

    (issue 145) added support for error settings

--- project/services/annotation_service.py
+++ project/services/annotation_service.py
@@ -9,20 +9,24 @@ def retrieve_annotation(annotation_code):
     annotation_info = db.session.query(Annotation, Annotator). \
         join(Annotator.annotations) \
         .filter(Annotation.id == annotation_code).first()
-
-    if not annotation_info:
+    annotation = Annotation.query.get(annotation_code)
+    if annotation is None:
         return None
+    if annotation.annotator_id is None:
+        name = None
+    else:
+        name = Annotator.query.get(annotation.annotator_id)
 
     annotation_dict = {
-        'id': annotation_info[0].id,
-        'x_center': annotation_info[0].x_center,
-        'y_center': annotation_info[0].y_center,
-        'width': annotation_info[0].width,
-        'height': annotation_info[0].height,
-        'class_id': annotation_info[0].class_id,
-        'project_id': annotation_info[0].project_id,
-        'image_id': annotation_info[0].image_id,
-        'annotator_name': annotation_info[1].name
+        'id': annotation.id,
+        'x_center': annotation.x_center,
+        'y_center': annotation.y_center,
+        'width': annotation.width,
+        'height': annotation.height,
+        'class_id': annotation.class_id,
+        'project_id': annotation.project_id,
+        'image_id': annotation.image_id,
+        'annotator_name': name
     }
 
     return annotation_dict
